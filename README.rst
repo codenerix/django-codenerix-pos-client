616c336735ccc21fa10cc852946c4a2c7084db9f
Juanmi Taboada <juanmi@centrologic.com>
2017-06-21 20:40:14 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,44 @@
+===========================
+django-codenerix-pos-client
+===========================
+
+Codenerix POS Client is a module that enables the system where it is installed to communicate with POS module from `CODENERIX.com <http://www.codenerix.com/>`_
+
+.. image:: http://www.centrologic.com/wp-content/uploads/2017/01/logo-codenerix.png
+    :target: http://www.codenerix.com
+    :alt: Try our demo with Centrologic Cloud
+
+****
+Demo
+****
+
+Coming soon...
+
+**********
+Quickstart
+**********
+
+1. Install this package::
+
+    For python 2: sudo pip2 install django-codenerix-pos-client
+    For python 3: sudo pip3 install django-codenerix-pos-client
+
+2. You are done, now you can work with your new command "codenerix_pos_client"
+
+*************
+Documentation
+*************
+
+Coming soon... do you help us? `Centrologic <http://www.centrologic.com/>`_
+
+******************
+Commercial support
+******************
+
+This project is backed by `Centrologic <http://www.centrologic.com/>`_. You can discover more in `CODENERIX.com <http://www.codenerix.com/>`_.
+If you need help implementing or hosting django-codenerix-pos-client, please contact us:
+http://www.centrologic.com/contacto/
+
+.. image:: http://www.centrologic.com/wp-content/uploads/2015/09/logo-centrologic.png
+    :target: http://www.centrologic.com
+    :alt: Centrologic is supported mainly by Centrologic Computational Logistic Center
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+import codenerix_pos_client
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-codenerix-pos-client',
+    version=codenerix_pos_client.__version__,
+    scripts=['codenerix_pos_client/codenerix_pos_client'],
+    include_package_data=True,
+    zip_safe=False,
+    license='Apache License Version 2.0',
+    description='Codenerix POS Client enables the system to work with codenerix_pos_client command line tool so it can connecto to CODENERIX POS server.',
+    long_description=README,
+    url='https://github.com/centrologic/django-codenerix-pos-client',
+    author=", ".join(codenerix_pos_client.__authors__),
+    keywords=['django', 'codenerix', 'management', 'erp', 'pos', 'client'],
+    platforms=['OS Independent'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[]
+)
