47669ee189c0e5a0bd81582322674279c364ab82
Juanmi Taboada <juanmi@centrologic.com>
2017-07-27 20:25:00 +0200

    Working example

--- codenerix_pos_client/posclient.py
+++ codenerix_pos_client/posclient.py
@@ -1,87 +1,91 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-""" POSClient 2 """
-
-import time
 import json
-import base64
-import pyotp
-import hashlib
 import uuid
-# import _thread
-# import time
 
 from ws4py.client.threadedclient import WebSocketClient
+
+from codenerix.lib.debugger import Debugger
+from codenerix_extensions.lib.cryptography import AESCipher
 
 # from manager import Manager
 from config import ID, KEY, SERVER
 
 
-# def on_message(ws, message):
-# def on_error(ws, error):
-#    if type(error) == KeyboardInterrupt:
-#        print("Closed by user request")
-#    else:
-#        print("### error ###")
-#        print(error)
+class POSClient(WebSocketClient, Debugger):
 
-# def on_close(ws):
-#    print("### closed ###")
-
-
-class POSClient(WebSocketClient):
-    def opened(self):
-        print("Open")
+    def __init__(self, *args, **kwargs):
+        self.crypto = AESCipher()
+        self.set_debug()
+        self.set_name('POSClient')
         self.challenge = None
+        super(POSClient, self).__init__(*args, **kwargs)
+
+    def opened(self):
+        self.debug("Opening Websocket", color="blue")
+        self.send({'action': 'get_hardware'})
 
     def closed(self, code, reason=None):
-        print("Closed down", code, reason)
+        self.debug("Websocket closed", color="blue")
 
-    def received_message(self, message):
-        # print(self.__dict__)
-        print()
-        print(message.__dict__)
-        request = json.loads(message.data.decode('utf-8'))
-        action = request.get('action', None)
-        if action == 'authenticate':
-            challenge = request.get('challenge', '')
-            self.challenge = challenge
+    def send_error(self, msg):
+        super(POSClient, self).send(json.dumps({'error': True, 'errortxt': msg}))
 
-            hashkey = "{}{}".format(challenge, KEY)
-            hash32 = base64.b32encode(bytes(hashkey, 'utf-8'))
-            totp = pyotp.TOTP(hash32).now()
+    def send(self, request):
+        # Encode request
+        msg = json.dumps(request)
 
-            token = hashlib.sha1(bytes("{}{}".format(hashkey, totp), 'utf-8')).hexdigest()
-            print("   > CHALLENGE: {}".format(challenge))
-            print("   > KEY:       {}".format(KEY))
-            print("   > HASHKEY:   {}".format(hashkey))
-            print("   > HASH32:    {}".format(hash32))
-            print("   > TOTP:      {}".format(totp))
-            print("   > TOKEN:     {}".format(token))
+        # Build query
+        query = {
+            'id': ID,
+            'message': self.crypto.encrypt(msg, KEY).decode('utf-8'),
+        }
 
-            msg = {
-                'action': 'authenticate',
-                'id': ID,
-                'token': token,
-                }
-            ws.send(json.dumps(msg))
-        elif action == 'authenticated':
-            authenticated = request.get('result')
-            print("Authenticated:{} - {}".format(authenticated, self.challenge))
-            # ws.send(json.dumps("HOLA"))
-            print("1")
-            ws.send(json.dumps({"action": "HOLA", 'id': 1}))
-            time.sleep(2)
-            print("2")
-            ws.send(json.dumps({"action": "HOLA", 'id': 1}))
-            time.sleep(2)
-            print("3")
-            ws.send(json.dumps({"action": "HOLA", 'id': 1}))
-            time.sleep(2)
-            print("4")
-    #        if authenticated:
+        # Encode to JSON
+        data = json.dumps(query)
 
+        # Send to remote
+        super(POSClient, self).send(data)
+
+    def received_message(self, package):
+        self.debug("New message arrived: {}".format(package), color='yellow')
+
+        try:
+            request = json.loads(package.data.decode('utf-8'))
+        except Exception:
+            request = None
+
+        # Check if we got msg
+        if request is not None and isinstance(request, dict):
+            message = request.get('message', None)
+            if message is not None:
+
+                # Decrypt message
+                msg = self.crypto.decrypt(message, KEY)
+                try:
+                    query = json.loads(msg)
+                except Exception:
+                    query = None
+                if query is not None and isinstance(query, dict):
+                    self.recv(query)
+                else:
+                    if query is None:
+                        self.send_error("Message is not JSON or is None")
+                    else:
+                        self.send_error("Message is not a Dictionary")
+            else:
+                self.send_error("Missing 'message' or is None")
+        else:
+            if request is None:
+                self.send_error("Request is not JSON or is None")
+            else:
+                self.send_error("Request is not a Dictionary")
+
+    def recv(self, message):
+        action = message.get('action', None)
+        if action == 'hola':
+            print("HOLA")
         else:
             print("Unknown action '{}'".format(action))
             # self.close(reason='Bye bye')
