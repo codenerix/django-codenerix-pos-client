6c14de0ec55fd8254804ea8ca0c953a62ed7a97c
Juanmi Taboada <juanmi@centrologic.com>
2017-08-31 18:28:28 +0200

    New shutdown system to protect ghost children

--- codenerix_pos_client/init.sh
+++ codenerix_pos_client/init.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+shutdown() {
+    # Get our process group id
+    PGID=$(ps -o pgid= $$ | grep -o [0-9]*)
+
+    # Kill it in a new new process group
+    setsid kill -- -$PGID
+    exit 0
+}
+
+trap "shutdown" SIGINT SIGTERM
+
 # Get local dir and make sure we are in it
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
